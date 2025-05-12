08d5819deb322d5b5c64dcc6ac334232d5fb3270
Findlay Dehring <s4120575@student.rmit.edu.au>
2025-05-13 09:20:55 +1000

    this is a message using the text editor in terminal. I got to this by commiting in the terminal without adding a message

--- java/GitTest.java
+++ java/GitTest.java
@@ -1,6 +1,12 @@
 public class GitTest{
     public static void main(String[] args){
         System.out.println("hello World!");
-        System.out.println("another line");
+        System.out.println("useful bash input for git");
+        System.out.printf("-    -git init- creates a new Git repository\r\n" + //
+                        "-  -git status- inspects the contents of the working directory and staging area\r\n" + //
+                        "-  -git add- adds files from the working directory to the staging area\r\n" + //
+                        "-  -git diff- shows the difference between the working directory and the staging area\r\n" + //
+                        "-  -git commit- permanently stores file changes from the staging area in the repository\r\n" + //
+                        "-  -git log- shows a list of all previous commits");
     }
 }
